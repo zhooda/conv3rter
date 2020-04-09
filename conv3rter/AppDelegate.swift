e1d98c66a2098578d78edded86100220817d0a72
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-04-08 22:19:02 -0600

    Initial Commit

---
+++ conv3rter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  conv3rter
+//
+//  Created by Zeeshan Hooda on 4/8/20.
+//  Copyright © 2020 Deceptive Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ conv3rter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  conv3rter
+//
+//  Created by Zeeshan Hooda on 4/8/20.
+//  Copyright © 2020 Deceptive Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
