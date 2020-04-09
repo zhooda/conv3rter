3537ba251202175943991869f811f11322e04187
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-04-09 16:41:21 -0600

    Base v0.1alpha

--- conv3rter/AppDelegate.swift
+++ conv3rter/AppDelegate.swift
@@ -11,7 +11,7 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -21,6 +21,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    // Quite the application instance when the window is closed
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
 }
 
--- conv3rter/ViewController.swift
+++ conv3rter/ViewController.swift
@@ -8,20 +8,75 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+class ViewController: NSViewController, NSWindowDelegate {
+    @IBOutlet weak var titleLabel: NSTextField!
+    @IBOutlet weak var versionLabel: NSTextField!
+    
+    @IBOutlet weak var decimalTextField: NSTextField!
+    @IBOutlet weak var binaryTextField: NSTextField!
+    @IBOutlet weak var hexTextField: NSTextField!
+    
+    private let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    private let plagueYear = 2020
+    
+    var decimalString : String {
+        get {
+            print(decimalTextField.stringValue)
+            return decimalTextField.stringValue
         }
     }
+    
+    @IBAction func testTypeAction(_ sender: Any) {
+        let testVar: String = (sender as AnyObject).stringValue
+        print(testVar)
+    }
+    
+    @IBAction func convertButtonAction(_ sender: Any) {
+        print("The button has been pressed")
+//        updateFieldPlaceholder(decimalTextField, "this is a test")
+        let decimal = UInt64(decimalTextField.doubleValue)
+        let binString = String(decimal, radix: 2)
+        let hexString = String(decimal, radix: 16)
+        
+        // UPDATE TEXT FIELD PLACEHOLDERS WITH CALCULATED VALUES
+        updateFieldPlaceholder(decimalTextField, String(decimal))
+        updateFieldPlaceholder(binaryTextField, binString)
+        updateFieldPlaceholder(hexTextField, hexString.uppercased())
+//        print(type(of: decimal))
+//        print(binString)
+//        print(hexString.uppercased())
+    }
+    
+    private func updateFieldPlaceholder(_ field: NSTextField, _ value: String) {
+        field.placeholderString = value
+    }
+    
+    override func viewDidLoad() {
+        let year = Calendar.current.component(.year, from: Date())
+        if (year == plagueYear) {
+            titleLabel.stringValue = "stay at h0me"
+        }
+        versionLabel.stringValue = "v" + appVersion!
+        // make broken warning
+        makeBrokenWarning()
+    }
+    
+    func makeBrokenWarning() {
+        let alert = NSAlert.init()
+        alert.messageText = "conv3rter v" + appVersion! + " is very broken."
+        alert.informativeText = "This application is very broken at the moment. Please only use the decimal text field as all the other text fields do not function at the moment kthxbye."
+        alert.addButton(withTitle: "Take me to crapville!")
+        alert.runModal()
+    }
+    
+    override func viewDidAppear() {
+//        let alert = NSAlert.init()
+//        alert.messageText = "VERY BROKEN"
+//        alert.informativeText = "This application is very broken at the moment. Please only use the decimal text field as all the other text fields do not function at the moment kthxbye."
+//        alert.addButton(withTitle: "Take me to crapville!")
+//        alert.runModal()
 
+    }
 
 }
 
